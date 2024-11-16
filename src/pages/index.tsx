906a04b00513309632d7c2c54456184c4f82cf93
flaviobernachia <flaviobernachia@gmail.com>
2024-11-16 01:16:23 +0000

    delete imports in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
+
 import Header from "@/components/header/header"
 import {HomePage} from "@/components/home/homePage"
 
